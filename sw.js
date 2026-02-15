d1dc4ccb21d84592e55db3313edc54debbc21aad
rutherfr12 <rutherfr@tcd.ie>
2026-02-15 13:03:18 +0800

    Update cache version to vocabsrs-cache-v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vocabsrs-cache-v1";
+const CACHE = "vocabsrs-cache-v2";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
